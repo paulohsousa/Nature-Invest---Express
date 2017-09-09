4afd0b5c22a44fb39b9532c9a9ee6b2e9ce0d438
victormf123 <victormf.df@hotmail.com>
2017-09-09 02:04:51 -0300

    first commit Nature Invest utilizando o express

---
+++ angular1/app/common/factories/handleResponseErrorFactory.js
@@ -0,0 +1,21 @@
+/**
+ * Created by matheus on 04/09/17.
+ */
+(function () {
+    angular.module('primeiraApp').factory('handleResponseError', [
+        '$q',
+        '$window',
+        'consts',
+        HandleResponseErrorFactory
+    ])
+    function HandleResponseErrorFactory($q, $window, consts) {
+        function responseError(errorResponse) {
+            if (errorResponse.status === 403) {
+                localStorage.removeItem(consts.userKey)
+                $window.location.href = '/'
+            }
+            return $q.reject(errorResponse)
+        }
+        return { responseError }
+    }
+})()
